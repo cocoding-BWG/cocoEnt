2aef59cd98833b8cb39334f17b3421b7e982f0fb
Joonkb <byeongjunkim@naver.com>
2023-03-05 11:06:10 +0900

    oracle local db 연결 세팅

---
+++ src/main/java/com/example/cocoEnt/connection/ConnectionConst.java
@@ -0,0 +1,7 @@
+package com.example.cocoEnt.connection;
+
+public abstract class ConnectionConst {
+    public static final String URL ="jdbc:oracle:thin:@127.0.0.1:1521:xe";
+    public static final String USERNAME = "cocoEnt";
+    public static final String PASSWORD = "cocoEnt";
+}
